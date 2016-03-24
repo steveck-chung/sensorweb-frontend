ed8dd09e0cb8b45b4bdc09ddd227a6f98e095bb8
Evan Xd <itoyxd@gmail.com>
2016-03-24 10:47:08 +0800

    Show sensor list in the webpage

--- js/sensor-management.js
+++ js/sensor-management.js
@@ -1,3 +1,20 @@
 (function(){
-
+  $.ajax({
+    url: 'evanxd/sensors',
+  })
+  .done(function(sensors) {
+    var sensorList = $('#sensor-list ul');
+    var html = '';
+    sensors.forEach(function(sensor) {
+      var html = '<li class="collection-item">' +
+                   '<div>' +
+                     sensor.name + '<a href="sensor-detail.html" class="secondary-content"><i class="material-icons">info</i></a>' +
+                   '</div>' +
+                 '</li>';
+      sensorList.append(html);
+    });
+  })
+  .fail(function(error) {
+    console.error(error);
+  })
 })();
