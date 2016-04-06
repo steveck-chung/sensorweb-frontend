c92ca8efd4e6b71d9f124089e44b2f58ce8519e9
steveck-chung <schung@mozilla.com>
2016-04-06 17:38:22 +0800

    Add user view

---
+++ js/user-detail.js
@@ -0,0 +1,59 @@
+(function(exports){
+
+  const SENSOR_LIST_MARKUP = '<a href=${url} class="collection-item">${name}</a>';
+  var fakeDataMode = true;
+
+  function renderSensorList(sensors) {
+    $.tmpl(SENSOR_LIST_MARKUP, sensors).appendTo("#sensor-list");
+  }
+
+  if (fakeDataMode) {
+    renderSensorList([
+      {
+        name: 'sensor1',
+        url: 'sensor-detail.html?id=1'
+      },
+      {
+        name: 'sensor2',
+        url: 'sensor-detail.html?id=2'
+      },
+      {
+        name: 'sensor3',
+        url: 'sensor-detail.html?id=3'
+      }
+    ]);
+  }
+
+  // Fetch sensor list
+  $.ajax({
+    url: 'sensors',
+  })
+  .done(function(sensors) {
+    latestSensors = sensors;
+    updateMap(latestSensors);
+  })
+  .fail(function(error) {
+    console.error(error);
+  });
+
+  // Fetch sensors for specific user
+  $.ajax({
+    url: 'projects/' + userId +  '/sensors',
+  })
+  .done(renderSensorList)
+  .fail(function(error) {
+    console.error(error);
+  });
+
+  // Fetch projects for the user
+  $.ajax({
+    url: 'projects',
+  })
+  .done(function(projects) {
+
+  })
+  .fail(function(error) {
+    console.error(error);
+  });
+
+})(window);
