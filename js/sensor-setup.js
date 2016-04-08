75d0e5856fe5b532021c10c4bb7325179141d3d2
steveck-chung <schung@mozilla.com>
2016-04-08 19:25:33 +0800

    Add location selection

--- js/sensor-setup.js
+++ js/sensor-setup.js
@@ -1,20 +1,32 @@
-(function(){
+(function(exports){
+  const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+  const API_KEY = '&key=AIzaSyAWlJoUn2DS8XUYilLXZE8dxYEXbo6dnaE';
+  const TOAST_DUR = 4000;
+
   var userId = $.url().param('userId');
   var sensorName = $('#sensor-name');
   var sensorLocation = $('#sensor-location');
   var sensorDescription = $('#sensor-description');
   var sensorProject = $('#sensor-project');
+  var sensorCoords;
+  var gMap;
 
   $('select').material_select();
 
   $('#setup-sensor').click(function() {
+    if (!sensorCoords) {
+      Materialize.toast('Please check your address on the map', TOAST_DUR);
+      return;
+    }
+
     var name = sensorName.val();
     $.post('sensors', {
       userId: userId,
       projectId: sensorProject.val(),
       name: name,
       description: sensorDescription.val(),
-      coords: sensorLocation.val()
+      address: sensorLocation.val(),
+      coords: sensorCoords
     })
     .done(function(result) {
       window.location = './sensor-management.html';
@@ -23,4 +35,52 @@
       console.error(err)
     });
   });
-})();
+
+  $('#check-addr-btn').click(function() {
+    var address = sensorLocation.val();
+
+    if (!address) {
+      return;
+    }
+
+    var formattedAddr = address.split(' ').join('+');
+    $.ajax({
+      url: GEOCODE_URL + formattedAddr + API_KEY,
+    })
+    .done(function(data) {
+      if (!data.results[0]) {
+        return;
+      }
+
+      sensorCoords = data.results[0].geometry.location;
+      gMap = new google.maps.Map(document.getElementById('location-map'), {
+        zoom: 16,
+        center: sensorCoords
+      });
+
+      var gMapMarker = new google.maps.Marker({
+        position: sensorCoords,
+        map: gMap,
+        draggable:true,
+        animation: google.maps.Animation.DROP
+      });
+
+      google.maps.event.addListener(gMapMarker, 'dragend', function() {
+        sensorCoords = gMapMarker.getPosition().toJSON();
+      });
+    })
+    .fail(function(error) {
+      console.error(error);
+    });
+  });
+
+  function initMap() {
+    gMap = new google.maps.Map(document.getElementById('location-map'), {
+      zoom: 1,
+      center: {lat: 0, lng: 0}
+    });
+  }
+
+
+  exports.initMap = initMap;
+})(window);
