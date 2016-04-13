96cc554821c9ee9554f31336aebe03e4f5741bfe
Evan Xd <itoyxd@gmail.com>
2016-04-13 14:46:44 +0800

    Clean up the code in index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,32 @@
 (function(){
   $('.button-collapse').sideNav();
-  //$('.parallax').parallax();
-  $(document).ready(function(){
-    $('.modal-trigger').leanModal();
-    $('select').material_select();
+  $('.modal-trigger').leanModal();
+  $('select').material_select();
+
+  $('#learn').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+          scrollTop: $("#what").offset().top - 64,
+          easing: "easeout"
+      }, 700);
+      return false;
   });
+
+  $('#subscribe').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+          scrollTop: $("#index-contact-banner").offset().top - 64,
+          easing: "easeout"
+      }, 700);
+      return false;
+  });
+
+  $('.modal-trigger').click(function(e){
+    $('input.select-dropdown').attr('placeholder','Pick any that applies');
+  });
+
   // XXX: A trick to enable the login feature.
   new Konami(function() {
     $('#nav').append('<li class="login modal-trigger"><a id="login-btn" href="#google-sign-in-modal">Log In</a></li>');
   });
 })();
-
-
-$('#learn').click(function(e){
-	e.preventDefault();
-	$('html, body').animate({
-        scrollTop: $("#what").offset().top-64,
-        easing: "easeout"
-    }, 700);
-    return false;
-});
-
-$('#subscribe').click(function(e){
-	e.preventDefault();
-	$('html, body').animate({
-        scrollTop: $("#index-contact-banner").offset().top-64,
-        easing: "easeout"
-    }, 700);
-    return false;
-});
-
-$('.modal-trigger').click(function(e){
-	console.log("!");
-	$('input.select-dropdown').attr('placeholder','Pick any that applies');
-});
