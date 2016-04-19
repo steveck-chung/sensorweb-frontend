fc149689959cbaab9570a3eaf527fb43f4c23dd3
Eddie Lin <yshlin@gmail.com>
2016-04-19 13:41:08 +0800

    Added pushpin menu and styles to doc pages.

--- js/documentation.js
+++ js/documentation.js
@@ -1,3 +1,25 @@
 (function(){
 
+	var $toc = $('.toc .collection');
+	var $tocItems = $toc.find('.collection-item');
+
+	$toc.pushpin({ top: $('.toc').offset().top, offset: 68});
+
+	$.localScroll({
+		queue:true,
+		duration:200,
+		hash:true,
+		onAfter: function(element) {
+		  	$tocItems.removeClass('active');
+			$toc.find('[href=#'+element.attr('id')+']').addClass('active');
+		}
+	});
+
+	var waypoints = $('h2, h3').waypoint({
+	  handler: function(direction) {
+	  	$tocItems.removeClass('active');
+	  	$toc.find('[href=#'+this.element.id+']').addClass('active');
+	  }
+	});
+
 })();
