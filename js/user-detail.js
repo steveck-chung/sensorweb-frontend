8d4d14cde904ce25a922bc158f2f048814d4d6a0
Evan Xd <itoyxd@gmail.com>
2016-04-20 09:45:17 +0800

    Rename the sensor-deail.html to sensor.html

--- js/user-detail.js
+++ js/user-detail.js
@@ -3,7 +3,7 @@
     $('.modal-trigger').leanModal();
   });
 
-  const SENSOR_LIST_MARKUP = '<a href="sensor-detail.html?id=${_id}" class="collection-item">${name}<button id="edit-device" class="waves-effect waves-light btn-large btn-control right" href="sensor-setup.html?userId=evanxd"><i class="material-icons left">mode_edit</i><span>Edit</span></button></a>';
+  const SENSOR_LIST_MARKUP = '<a href="sensor.html?id=${_id}" class="collection-item">${name}<button id="edit-device" class="waves-effect waves-light btn-large btn-control right" href="sensor-setup.html?userId=evanxd"><i class="material-icons left">mode_edit</i><span>Edit</span></button></a>';
   const PROJECT_LIST_MARKUP = '<div class="col s6 m3"><a href="project-detail.html?id=${id}"><div class="card"><div class="card-image"></div></div><p class="center-align">${name}</p></a></div>';
   const fakeDataMode = false;
   var userId;
